0d1e84ea719707596d8b669f14c0cde3dd6f4e91
MateoMat <m.maslank@interia.pl>
2018-07-22 23:56:31 +0200

    MySql Connection Class added

---
+++ Model/MySQLConnect.php
@@ -0,0 +1,14 @@
+<?php
+//Class responsible for MySql Connection
+class Database{
+    static protected $dbUser='root';
+    static protected $dbPass='root';
+    static protected $dbName='APManager';
+    static protected $dbHost='localhost';
+    
+    static function createConnection(){
+        return $conn= new mysqli(self::$dbHost, self::$dbUser, self::$dbPass, self::$dbName);
+    }
+}
+
+
